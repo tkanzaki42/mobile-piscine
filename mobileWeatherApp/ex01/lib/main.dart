e09bf03766a6d6717e0d62f655273f18da8e2596
tkanzaki42 <tkanzaki@student.42tokyo.jp>
2024-04-27 23:14:05 +0900

    ex01

---
+++ mobileWeatherApp/ex01/lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 128, 71, 228)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+ int _currentIndex = 0;
+  final PageController _pageController = PageController();
+  TextEditingController _textController = TextEditingController();
+  String _displayedText = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: _textController,
+          decoration: InputDecoration(
+            hintText: 'Search',
+            suffixIcon: IconButton(
+              icon: Icon(Icons.location_on),
+              onPressed: () {
+
+                setState(() {
+                  _displayedText = 'Tokyo';
+                });
+              },
+            ),
+          ),
+          onChanged: (text) {
+            setState(() {
+              _displayedText = text;
+            });
+          },
+        ),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        children: <Widget>[
+          _buildPage('Currently'),
+          _buildPage('Today'),
+          _buildPage('Weekly'),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          _pageController.animateToPage(
+            index,
+            duration: Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+          );
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.today),
+            label: 'Currently',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Today',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_view_week),
+            label: 'Weekly',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPage(String tabName) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center, // 縦方向の中心に配置
+        children: <Widget>[
+          Text(
+            tabName,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+          ),
+          if (_displayedText.isNotEmpty)
+            Text(
+              _displayedText,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
